6617000cc3c36a2982f0610a25931aa01a4409cf
CODEKRUNCH <awaisshahzadpk@protonmail.com>
2025-10-21 10:20:04 -0700

    Old Backend(Extra Deleted)

--- Backend_New/Backend/Transactions/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Transactionrecord
-# Register your models here.
-
-admin.site.register(Transactionrecord)
-# Register your models here.
--- Backend_New/Backend/Transactions/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class TransactionsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Transactions'
-    def ready(self):
-        import Transactions.signals  # Ensures your signal receivers are imported
